1726f3a2a4eb1fcd27501853d2382e2c4e5d3a98
hwvnkim <53571695+hwvnkim@users.noreply.github.com>
2021-10-06 23:14:39 +0900

    Update HIndex (P42747, Case1).py

--- DH/HIndex (P42747, Case1).py
+++ DH/HIndex (P42747, Case1).py
@@ -1,3 +1,9 @@
+# 작성일 : 2021.10.06
+# 언어 : Python
+# 출처 : 프로그래머스 [코딩테스트 연습>정렬>H-Index]
+# 메모 : 파이썬의 리스트 sort 및 reverse 기능을 활용하여 해결했습니다 
+#       (더 좋은 기능이 있을법 한데...자바로 풀며 고민해보겠읍니당)
+
 def solution(citations):
     citations.sort()
     citations.reverse()
