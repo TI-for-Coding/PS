917927208b1678654c4780d94222ff4757e5010d
hwvnkim <53571695+hwvnkim@users.noreply.github.com>
2021-10-06 23:33:25 +0900

    Update TruckOnBridge(P42583, case1).java

--- DH/TruckOnBridge(P42583, case1).java
+++ DH/TruckOnBridge(P42583, case1).java
@@ -25,26 +25,26 @@ class Solution {
         
         while (true) {
             
-            answer++;
+            answer++;   // = time
             
-            if (truck_ops_weights.size() != 0) {
-                if(truck_ops_time.get(0) >= bridge_length) {
+            if (truck_ops_weights.size() != 0) {                //다리를 지나는 트럭이 있는 경우
+                if(truck_ops_time.get(0) >= bridge_length) {    //최우선의 트럭이 다리를 지났는지 확인하여, 대상 pop
                     truck_ops_weights.remove(0);
                     truck_ops_time.remove(0);
                 }
             }
             
-            if (truck_weight_list.size() != 0) {
-                if (sumList(truck_ops_weights) + truck_weight_list.get(0) <= weight) {
-                    truck_ops_weights.add(truck_weight_list.get(0));
+            if (truck_weight_list.size() != 0) {                                            // 다리를 지나야 할 트럭이 있는 경우
+                if (sumList(truck_ops_weights) + truck_weight_list.get(0) <= weight) {      // 대상 트럭이 다리를 지날 수 있는지 확인 (메소드 sumList 참고)
+                    truck_ops_weights.add(truck_weight_list.get(0));                        // 다리를 지날 수 있는 트럭을 push
                     truck_weight_list.remove(0);
                     truck_ops_time.add(0);
                     }
                 }
             
-            addList(truck_ops_time, 1);
+            addList(truck_ops_time, 1);     // 메소드 addList 참고
              
-            if ((truck_ops_weights.size() == 0)&&(truck_weight_list.size() == 0)) {
+            if ((truck_ops_weights.size() == 0)&&(truck_weight_list.size() == 0)) {         // 다리를 지나야 할 트럭, 다리를 지나는 트럭이 모두 없으면, 종료
                 break;
             }
             
@@ -53,20 +53,18 @@ class Solution {
         return answer;
     }
     
-    public static void addList (List <Integer> list, int value) {
+    public static void addList (List <Integer> list, int value) {   // 정수형 리스트 list에, 정수 value를 모든 원소에 더하는 메소드
         for (int i = 0; i < list.size(); i++) {
             list.add(list.get(0) + value);
             list.remove(0);
         }
     }
     
-    public static int sumList (List <Integer> list) {
-        
+    public static int sumList (List <Integer> list) {   // 정수형 리스트 list의 모든 원소의 합을 출력하는 메소드
         int result = 0;
         for (int i = 0; i < list.size(); i++) {
             result = result + list.get(i);
         }
-        
         return result;
     }
     
