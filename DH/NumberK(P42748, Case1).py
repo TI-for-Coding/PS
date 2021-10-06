7c333ca5279fb4aa1dd0273f6a8f4a1d01d917ff
hwvnkim <53571695+hwvnkim@users.noreply.github.com>
2021-10-06 23:27:48 +0900

    Update NumberK(P42748, Case1).py

--- DH/NumberK(P42748, Case1).py
+++ DH/NumberK(P42748, Case1).py
@@ -6,7 +6,7 @@
 def solution(array, commands):
     answer = []
     for que in commands :
-        target_arr = array[(que[0] - 1):que[1]]
+        target_arr = array[(que[0] - 1):que[1]]     # Substring
         target_arr.sort()
-        answer.append(target_arr[que[2] - 1])
+        answer.append(target_arr[que[2] - 1])       # K번째 수를 리스트 answer에 append
     return answer
