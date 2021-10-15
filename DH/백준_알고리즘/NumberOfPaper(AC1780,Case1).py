f5106183fd023bb950c79e695fde6550d6b6cd58
hwvnkim <53571695+hwvnkim@users.noreply.github.com>
2021-10-15 23:23:26 +0900

    Update NumberOfPaper(AC1780,Case1).py

--- DH/백준_알고리즘/NumberOfPaper(AC1780,Case1).py
+++ DH/백준_알고리즘/NumberOfPaper(AC1780,Case1).py
@@ -1,4 +1,11 @@
-
+# 작성일 : 2021.10.14
+# 언어 : Python
+# 출처 : ACMICPC(백준) [1780번 : 종이의 개수]
+# 메모 : 재귀 함수를 통한 분할과 정복으로 문제 풀이
+#       1) 주어진 면적 (초기값은 N X N) 을 일일이 읽어가며, 동일한 색상인지 확인
+#       2) 동일한 색상이 확인될 경우, result 배열에 +1
+#       3) 다른 색상이 발견되었을 경우, 주어진 면적을 3분할하여, 1)번의 재귀 함수 수행 (주어진 면적인 N//3 X N//3)
+#       4) 최종적으로 주어진 면적이 한 칸인 경우에는, for 문을 수행하지 않으므로 result 배열에 각각 +1
 
 def search(x, y, N) :
 
@@ -20,6 +27,7 @@ result = [0, 0, 0]
 paper = [list(map(int, input().split(' '))) for i in range (N)]
 
 search(0, 0, N)
+
 print(result[-1])
 print(result[0])
 print(result[1])
