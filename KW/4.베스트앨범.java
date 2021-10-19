aad4da9dd1fd29da9ed40b578889b18d6d135689
KunWoong <ygw77@naver.com>
2021-10-19 16:46:10 +0900

    베스트앨범

---
+++ KW/4.베스트앨범.java
@@ -0,0 +1,79 @@
+/*
+
+최대한 Stream을 이용하려하였으나, Stream api에 미숙하여..
+혹시 개선점을 발견하신다면, 말씀해주시면 감사하겠습니다.
+
+ */
+
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(String[] genres, int[] plays) {
+        List<Integer> answer = new ArrayList<>();
+
+        List<MusicRecord> records = IntStream.range(0, genres.length)
+                .mapToObj(idx -> new MusicRecord(idx, genres[idx], plays[idx]))
+                .collect(Collectors.toList());
+
+        Map<String, List<MusicRecord>> result =
+                records.stream().collect(
+                        Collectors.groupingBy(
+                                MusicRecord::getGenre,
+                                Collectors.mapping(
+                                        Function.identity(),
+                                        Collectors.collectingAndThen(
+                                                Collectors.toList(),
+                                                musicRecords -> musicRecords.stream().sorted().limit(2).collect(Collectors.toList())
+                                        )
+
+                                )
+                        )
+                );
+        List<Map.Entry<String, Integer>> sortedGenre = records.stream().collect(Collectors.groupingBy(MusicRecord::getGenre, Collectors.summingInt(MusicRecord::getCount)))
+                .entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toList());
+
+        sortedGenre.forEach(entry -> {
+            answer.addAll(result.get(entry.getKey()).stream().map(MusicRecord::getIndex).collect(Collectors.toList()));
+        });
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
+
+
+class MusicRecord implements Comparable<MusicRecord>{
+    public final int index;
+    public final String genre;
+    public final int count;
+
+    MusicRecord(int index, String genre, int count) {
+        this.index = index;
+        this.genre = genre;
+        this.count = count;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public int compareTo(MusicRecord o) {
+        if(this.count > o.count) return -1;
+        else if(this.count == o.count) {
+            return Integer.compare(this.index, o.index);
+        }else return 1;
+    }
+
+}
